49d5b4fe7f18ea112f2ffb3b6ed6ad5e8da31982
Juansu01 <thejuank0@hotmail.com>
2021-07-21 12:09:43 -0500

    Modified code so that it handles already existing left nodes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node_at_left;
+	binary_tree_t *old_node_at_left;
 
 	if (parent == NULL)
 	{
@@ -21,8 +22,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	if (parent->left != NULL)
+	{
 
-	parent->left = node_at_left;
+		old_node_at_left = parent->left;
+		parent->left = node_at_left;
+		node_at_left->left = old_node_at_left;
+		old_node_at_left->parent = node_at_left;
+	}
+	else
+	{
+		parent->left = node_at_left;
+	}
 
 	return (node_at_left);
 }
