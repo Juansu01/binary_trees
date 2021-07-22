6663a19c89f7edb6fc9d3692e75b5b4be38b9990
anamariaroman <2979@holbertonschool.com>
2021-07-21 23:47:52 -0500

    task 7 more

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_preorder - function that goes through a binary tree
- * @tree: pointer to the root node
- * @func: pointer to a function to call
- * Return: Nothing.
+ * binary_tree_preorder - function that goes through binary tree in-order
+ * @tree: ptr to the root node
+ * @func: ptr to a function to call
+ * Return: Void.
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
